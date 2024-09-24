708d13a953dc9fe929d191c164df0201e513d1fe
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2024-09-24 09:01:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,29 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
-    return view('home', ['title'  => 'Home Page']);
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
     return view('about', ['name' => 'Franshel Tranetha', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title'  => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);       
 });
 
+Route::get('/posts/{slug}', function($slug){
+
+     $post = Post::find($slug);
+     
+    return view('post', ['title' => 'Single Post', 'post' => $post]); 
+});
+
+
 Route::get('/contact', function () {
-    return view('contact', ['title'  => 'Contact']);
+    return view('contact', ['title' => 'Contact']);
 });
