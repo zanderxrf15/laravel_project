7888e617285166dc5878431db24d5ad71b44b79f
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2024-09-30 17:07:11 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    $this->call([CategorySeeder::class, UserSeeder::class]);
+       Post::factory(100)->recycle([
+         Category::all(),
+         User::all()
+       ])->create();
     }
 }
