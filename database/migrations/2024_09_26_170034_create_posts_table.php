af9f10a056e4bf56864f35f47e6dd35facf857ae
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2024-09-30 17:16:41 +0700

    Update 2024_09_26_170034_create_posts_table.php

--- database/migrations/2024_09_26_170034_create_posts_table.php
+++ database/migrations/2024_09_26_170034_create_posts_table.php
@@ -14,7 +14,14 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
+            $table->foreignId('author_id')->constrained(
+                table:'users',
+                indexName:'posts_author_id',
+            );
+            $table->foreignId('category_id')->constrained(
+                table:'categories',
+                indexName:'posts_category_id',
+            );
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
