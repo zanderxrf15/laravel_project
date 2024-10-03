d53f1246e8a6f006f4a3f6cca3052b0af1fdb96a
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2024-10-04 01:32:51 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = Post::latest()->get();
-    return view('posts', ['title' => 'Blog', 'posts' => $posts)]);       
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);       
 });
 
 Route::get('/posts/{post:slug}', function(Post $post){  
